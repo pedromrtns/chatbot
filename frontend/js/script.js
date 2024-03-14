9ab3364e2bdbda2cc79ae9e37c74491ecbd42dc1
pedromrtns <pcruz6064@gmail.com>
2024-03-14 00:38:41 -0300

    server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -84,7 +84,7 @@ const handleLogin = (event) => {
     login.style.display = "none";
     chat.style.display = "flex";
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chatbot-backend-xchc.onrender.com")
     websocket.onmessage = processMessage
 
     // console.log(Math.round(Math.random() * 10))
